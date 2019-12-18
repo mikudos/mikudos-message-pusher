36512f89a1ef5f20dd6f9147f7dba83880d0e6d7
yueguanyu <yue.guanyu@hotmail.com>
2019-12-18 21:17:08 +0800

    prepare for channel

--- handler/handlers.go
+++ handler/handlers.go
@@ -9,50 +9,74 @@ import (
 	pb "github.com/mikudos/mikudos-message-pusher/proto/message-pusher"
 )
 
+// Server implement Message-Pusher Server
 type Server struct {
-	Recv chan *pb.Message
-	Resp chan *pb.Response
-	Done chan bool
+	Recv     chan *pb.Message
+	Resp     chan *pb.Response
+	Returned map[string]map[int64]chan *pb.Response
+	Done     chan bool
 }
 
+// PushToChannel push message to the message Gate
 func (s *Server) PushToChannel(ctx context.Context, req *pb.Message) (*pb.Response, error) {
 	s.Recv <- req
-	msg := &pb.Response{}
-	return msg, nil
+	res := &pb.Response{MsgId: req.MsgId, ChannelId: req.ChannelId}
+	return res, nil
 }
 
+// PushToChannelWithStatus push message to the message Gate and wait for result
 func (s *Server) PushToChannelWithStatus(ctx context.Context, req *pb.Message) (*pb.Response, error) {
 	s.Recv <- req
+	mid := req.GetMsgId()
+	channelID := req.GetChannelId()
+	if s.Returned[channelID] == nil {
+		s.Returned[channelID] = map[int64]chan *pb.Response{mid: make(chan *pb.Response, 1)}
+	} else if s.Returned[channelID][mid] == nil {
+		s.Returned[channelID][mid] = make(chan *pb.Response, 1)
+	}
 	for {
-		resp := <-s.Resp
-		if resp.Msg == "" {
-			return resp, nil
+		ret := <-s.Returned[channelID][mid]
+		delete(s.Returned[channelID], mid)
+		if len(s.Returned[channelID]) == 0 {
+			delete(s.Returned, channelID)
 		}
+		return ret, nil
 	}
 }
 
+// GateStream gate stream communication
 func (s *Server) GateStream(stream pb.MessagePusher_GateStreamServer) (err error) {
 	fmt.Printf("Received GateStream request\n")
 
 	go func() {
-		select {
-		case <-s.Done:
-			return
-		case msg := <-s.Recv:
-			stream.Send(msg)
+		defer fmt.Printf("GateStream break\n")
+		for {
+			select {
+			case <-s.Done:
+				return
+			case msg := <-s.Recv:
+				stream.Send(msg)
+				break
+			case resp := <-s.Resp:
+				fmt.Printf("receive Gate response: %v\n", resp)
+				break
+			}
 		}
 	}()
 
 	for {
 		resp, err := stream.Recv()
 		if err == io.EOF {
+			s.Done <- true
 			break
 		}
 		if err != nil {
+			s.Done <- true
+			break
 			log.Fatalf("Error while reading client stream: %v", err)
 		}
 		s.Resp <- resp
 	}
-
+	fmt.Printf("GateStream break\n")
 	return err
 }
--- proto/message-pusher/message-pusher.pb.go
+++ proto/message-pusher/message-pusher.pb.go
@@ -3,14 +3,13 @@
 
 package message_pusher
 
+import proto "github.com/golang/protobuf/proto"
+import fmt "fmt"
+import math "math"
+
 import (
-	context "context"
-	fmt "fmt"
-	proto "github.com/golang/protobuf/proto"
+	context "golang.org/x/net/context"
 	grpc "google.golang.org/grpc"
-	codes "google.golang.org/grpc/codes"
-	status "google.golang.org/grpc/status"
-	math "math"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -22,12 +21,13 @@ var _ = math.Inf
 // is compatible with the proto package it is being compiled against.
 // A compilation error at this line likely means your copy of the
 // proto package needs to be updated.
-const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
+const _ = proto.ProtoPackageIsVersion2 // please upgrade the proto package
 
 type Message struct {
-	Msg                  []byte   `protobuf:"bytes,1,opt,name=msg,proto3" json:"msg,omitempty"`
-	MsgId                int64    `protobuf:"varint,2,opt,name=msgId,proto3" json:"msgId,omitempty"`
-	GroupId              string   `protobuf:"bytes,3,opt,name=groupId,proto3" json:"groupId,omitempty"`
+	Id                   int64    `protobuf:"varint,1,opt,name=id,proto3" json:"id,omitempty"`
+	Msg                  []byte   `protobuf:"bytes,2,opt,name=msg,proto3" json:"msg,omitempty"`
+	ChannelId            string   `protobuf:"bytes,3,opt,name=channelId,proto3" json:"channelId,omitempty"`
+	MsgId                int64    `protobuf:"varint,4,opt,name=msgId,proto3" json:"msgId,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
@@ -37,17 +37,16 @@ func (m *Message) Reset()         { *m = Message{} }
 func (m *Message) String() string { return proto.CompactTextString(m) }
 func (*Message) ProtoMessage()    {}
 func (*Message) Descriptor() ([]byte, []int) {
-	return fileDescriptor_23ef910bf9ada3da, []int{0}
+	return fileDescriptor_message_pusher_2ba676d00d641347, []int{0}
 }
-
 func (m *Message) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_Message.Unmarshal(m, b)
 }
 func (m *Message) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_Message.Marshal(b, m, deterministic)
 }
-func (m *Message) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_Message.Merge(m, src)
+func (dst *Message) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_Message.Merge(dst, src)
 }
 func (m *Message) XXX_Size() int {
 	return xxx_messageInfo_Message.Size(m)
@@ -58,6 +57,13 @@ func (m *Message) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_Message proto.InternalMessageInfo
 
+func (m *Message) GetId() int64 {
+	if m != nil {
+		return m.Id
+	}
+	return 0
+}
+
 func (m *Message) GetMsg() []byte {
 	if m != nil {
 		return m.Msg
@@ -65,18 +71,18 @@ func (m *Message) GetMsg() []byte {
 	return nil
 }
 
+func (m *Message) GetChannelId() string {
+	if m != nil {
+		return m.ChannelId
+	}
+	return ""
+}
+
 func (m *Message) GetMsgId() int64 {
 	if m != nil {
 		return m.MsgId
 	}
 	return 0
-}
-
-func (m *Message) GetGroupId() string {
-	if m != nil {
-		return m.GroupId
-	}
-	return ""
 }
 
 type Request struct {
@@ -90,17 +96,16 @@ func (m *Request) Reset()         { *m = Request{} }
 func (m *Request) String() string { return proto.CompactTextString(m) }
 func (*Request) ProtoMessage()    {}
 func (*Request) Descriptor() ([]byte, []int) {
-	return fileDescriptor_23ef910bf9ada3da, []int{1}
+	return fileDescriptor_message_pusher_2ba676d00d641347, []int{1}
 }
-
 func (m *Request) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_Request.Unmarshal(m, b)
 }
 func (m *Request) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_Request.Marshal(b, m, deterministic)
 }
-func (m *Request) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_Request.Merge(m, src)
+func (dst *Request) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_Request.Merge(dst, src)
 }
 func (m *Request) XXX_Size() int {
 	return xxx_messageInfo_Request.Size(m)
@@ -119,7 +124,10 @@ func (m *Request) GetName() string {
 }
 
 type Response struct {
-	Msg                  string   `protobuf:"bytes,1,opt,name=msg,proto3" json:"msg,omitempty"`
+	MsgId                int64    `protobuf:"varint,1,opt,name=msgId,proto3" json:"msgId,omitempty"`
+	ChannelId            string   `protobuf:"bytes,2,opt,name=channelId,proto3" json:"channelId,omitempty"`
+	Received             bool     `protobuf:"varint,3,opt,name=received,proto3" json:"received,omitempty"`
+	Request              bool     `protobuf:"varint,4,opt,name=request,proto3" json:"request,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
@@ -129,17 +137,16 @@ func (m *Response) Reset()         { *m = Response{} }
 func (m *Response) String() string { return proto.CompactTextString(m) }
 func (*Response) ProtoMessage()    {}
 func (*Response) Descriptor() ([]byte, []int) {
-	return fileDescriptor_23ef910bf9ada3da, []int{2}
+	return fileDescriptor_message_pusher_2ba676d00d641347, []int{2}
 }
-
 func (m *Response) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_Response.Unmarshal(m, b)
 }
 func (m *Response) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_Response.Marshal(b, m, deterministic)
 }
-func (m *Response) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_Response.Merge(m, src)
+func (dst *Response) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_Response.Merge(dst, src)
 }
 func (m *Response) XXX_Size() int {
 	return xxx_messageInfo_Response.Size(m)
@@ -150,11 +157,32 @@ func (m *Response) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_Response proto.InternalMessageInfo
 
-func (m *Response) GetMsg() string {
+func (m *Response) GetMsgId() int64 {
 	if m != nil {
-		return m.Msg
+		return m.MsgId
+	}
+	return 0
+}
+
+func (m *Response) GetChannelId() string {
+	if m != nil {
+		return m.ChannelId
 	}
 	return ""
+}
+
+func (m *Response) GetReceived() bool {
+	if m != nil {
+		return m.Received
+	}
+	return false
+}
+
+func (m *Response) GetRequest() bool {
+	if m != nil {
+		return m.Request
+	}
+	return false
 }
 
 type StreamingRequest struct {
@@ -168,17 +196,16 @@ func (m *StreamingRequest) Reset()         { *m = StreamingRequest{} }
 func (m *StreamingRequest) String() string { return proto.CompactTextString(m) }
 func (*StreamingRequest) ProtoMessage()    {}
 func (*StreamingRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_23ef910bf9ada3da, []int{3}
+	return fileDescriptor_message_pusher_2ba676d00d641347, []int{3}
 }
-
 func (m *StreamingRequest) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_StreamingRequest.Unmarshal(m, b)
 }
 func (m *StreamingRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_StreamingRequest.Marshal(b, m, deterministic)
 }
-func (m *StreamingRequest) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_StreamingRequest.Merge(m, src)
+func (dst *StreamingRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_StreamingRequest.Merge(dst, src)
 }
 func (m *StreamingRequest) XXX_Size() int {
 	return xxx_messageInfo_StreamingRequest.Size(m)
@@ -207,17 +234,16 @@ func (m *Ping) Reset()         { *m = Ping{} }
 func (m *Ping) String() string { return proto.CompactTextString(m) }
 func (*Ping) ProtoMessage()    {}
 func (*Ping) Descriptor() ([]byte, []int) {
-	return fileDescriptor_23ef910bf9ada3da, []int{4}
+	return fileDescriptor_message_pusher_2ba676d00d641347, []int{4}
 }
-
 func (m *Ping) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_Ping.Unmarshal(m, b)
 }
 func (m *Ping) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_Ping.Marshal(b, m, deterministic)
 }
-func (m *Ping) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_Ping.Merge(m, src)
+func (dst *Ping) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_Ping.Merge(dst, src)
 }
 func (m *Ping) XXX_Size() int {
 	return xxx_messageInfo_Ping.Size(m)
@@ -246,17 +272,16 @@ func (m *Pong) Reset()         { *m = Pong{} }
 func (m *Pong) String() string { return proto.CompactTextString(m) }
 func (*Pong) ProtoMessage()    {}
 func (*Pong) Descriptor() ([]byte, []int) {
-	return fileDescriptor_23ef910bf9ada3da, []int{5}
+	return fileDescriptor_message_pusher_2ba676d00d641347, []int{5}
 }
-
 func (m *Pong) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_Pong.Unmarshal(m, b)
 }
 func (m *Pong) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_Pong.Marshal(b, m, deterministic)
 }
-func (m *Pong) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_Pong.Merge(m, src)
+func (dst *Pong) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_Pong.Merge(dst, src)
 }
 func (m *Pong) XXX_Size() int {
 	return xxx_messageInfo_Pong.Size(m)
@@ -281,32 +306,6 @@ func init() {
 	proto.RegisterType((*StreamingRequest)(nil), "message_pusher.StreamingRequest")
 	proto.RegisterType((*Ping)(nil), "message_pusher.Ping")
 	proto.RegisterType((*Pong)(nil), "message_pusher.Pong")
-}
-
-func init() {
-	proto.RegisterFile("proto/message-pusher/message-pusher.proto", fileDescriptor_23ef910bf9ada3da)
-}
-
-var fileDescriptor_23ef910bf9ada3da = []byte{
-	// 287 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x9c, 0x51, 0xdd, 0x4a, 0xc3, 0x30,
-	0x14, 0x5e, 0xec, 0x7e, 0xec, 0x41, 0xc7, 0x08, 0xc3, 0x95, 0xa1, 0x52, 0x72, 0x55, 0x2f, 0xac,
-	0xa2, 0x8f, 0xa0, 0x22, 0x43, 0x26, 0x23, 0x13, 0xbc, 0x94, 0xe8, 0x42, 0x5a, 0xb4, 0x49, 0xed,
-	0x49, 0x1e, 0xdb, 0x77, 0x10, 0xd3, 0xf6, 0xa2, 0x83, 0xde, 0x78, 0x77, 0xbe, 0x9f, 0x7c, 0x87,
-	0x7c, 0x07, 0x2e, 0xca, 0xca, 0x58, 0x73, 0x55, 0x48, 0x44, 0xa1, 0xe4, 0x65, 0xe9, 0x30, 0x93,
-	0xd5, 0x1e, 0x4c, 0xbd, 0x87, 0x4e, 0x1b, 0xf6, 0xad, 0x66, 0xd9, 0x13, 0x4c, 0xd6, 0x35, 0x43,
-	0x67, 0x10, 0x14, 0xa8, 0x22, 0x12, 0x93, 0xe4, 0x88, 0xff, 0x8d, 0x74, 0x0e, 0xa3, 0x02, 0xd5,
-	0x6a, 0x17, 0x1d, 0xc4, 0x24, 0x09, 0x78, 0x0d, 0x68, 0x04, 0x13, 0x55, 0x19, 0x57, 0xae, 0x76,
-	0x51, 0x10, 0x93, 0x24, 0xe4, 0x2d, 0x64, 0x67, 0x30, 0xe1, 0xf2, 0xdb, 0x49, 0xb4, 0x94, 0xc2,
-	0x50, 0x8b, 0x42, 0xfa, 0xb4, 0x90, 0xfb, 0x99, 0x9d, 0xc2, 0x21, 0x97, 0x58, 0x1a, 0x8d, 0x9d,
-	0x65, 0xa1, 0x5f, 0xc6, 0x12, 0x98, 0x6d, 0x6d, 0x25, 0x45, 0x91, 0x6b, 0xd5, 0xa6, 0xcc, 0x61,
-	0xf4, 0x61, 0x9c, 0xb6, 0xde, 0x17, 0xf0, 0x1a, 0xb0, 0x73, 0x18, 0x6e, 0x72, 0xad, 0xe8, 0x09,
-	0x8c, 0xd1, 0x56, 0xe6, 0x53, 0x36, 0x72, 0x83, 0xbc, 0x6e, 0xfa, 0xf5, 0x9b, 0x1f, 0x02, 0xd3,
-	0x75, 0xa7, 0x06, 0x7a, 0x0f, 0xc7, 0x1b, 0x87, 0xd9, 0x8b, 0xb9, 0xcb, 0x84, 0xd6, 0xf2, 0x8b,
-	0x2e, 0xd2, 0x6e, 0x51, 0x69, 0xf3, 0x60, 0x19, 0xed, 0x0b, 0xed, 0x97, 0xd8, 0x80, 0x3e, 0xc3,
-	0xa2, 0x93, 0xf2, 0x9a, 0xdb, 0x6c, 0x6b, 0x85, 0x75, 0xf8, 0xbf, 0xbc, 0x07, 0x80, 0x47, 0x61,
-	0x65, 0x5d, 0x0b, 0xed, 0x75, 0x2e, 0xfb, 0xc2, 0xd9, 0x20, 0x21, 0xd7, 0xe4, 0x7d, 0xec, 0x4f,
-	0x7f, 0xfb, 0x1b, 0x00, 0x00, 0xff, 0xff, 0xdb, 0x4c, 0xb5, 0x3b, 0x27, 0x02, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -388,20 +387,6 @@ type MessagePusherServer interface {
 	PushToChannel(context.Context, *Message) (*Response, error)
 	PushToChannelWithStatus(context.Context, *Message) (*Response, error)
 	GateStream(MessagePusher_GateStreamServer) error
-}
-
-// UnimplementedMessagePusherServer can be embedded to have forward compatible implementations.
-type UnimplementedMessagePusherServer struct {
-}
-
-func (*UnimplementedMessagePusherServer) PushToChannel(ctx context.Context, req *Message) (*Response, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method PushToChannel not implemented")
-}
-func (*UnimplementedMessagePusherServer) PushToChannelWithStatus(ctx context.Context, req *Message) (*Response, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method PushToChannelWithStatus not implemented")
-}
-func (*UnimplementedMessagePusherServer) GateStream(srv MessagePusher_GateStreamServer) error {
-	return status.Errorf(codes.Unimplemented, "method GateStream not implemented")
 }
 
 func RegisterMessagePusherServer(s *grpc.Server, srv MessagePusherServer) {
@@ -492,4 +477,33 @@ var _MessagePusher_serviceDesc = grpc.ServiceDesc{
 		},
 	},
 	Metadata: "proto/message-pusher/message-pusher.proto",
+}
+
+func init() {
+	proto.RegisterFile("proto/message-pusher/message-pusher.proto", fileDescriptor_message_pusher_2ba676d00d641347)
+}
+
+var fileDescriptor_message_pusher_2ba676d00d641347 = []byte{
+	// 329 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x9c, 0x52, 0x4d, 0x4f, 0xeb, 0x30,
+	0x10, 0xac, 0xd3, 0xaf, 0x74, 0xf5, 0x5e, 0x55, 0xad, 0xaa, 0xd7, 0xa8, 0x7a, 0xa0, 0xca, 0xa7,
+	0x70, 0xa0, 0x20, 0xf8, 0x09, 0x80, 0x50, 0x0f, 0x45, 0x95, 0x8b, 0xc4, 0xb1, 0x0a, 0xcd, 0x2a,
+	0x89, 0x20, 0x76, 0x89, 0x1d, 0xfe, 0x35, 0xff, 0x01, 0x61, 0x27, 0x94, 0x54, 0xea, 0x85, 0xdb,
+	0xce, 0xcc, 0x7a, 0x66, 0xbd, 0x36, 0x9c, 0xed, 0x0a, 0x65, 0xd4, 0x45, 0x4e, 0x5a, 0x47, 0x09,
+	0x9d, 0xef, 0x4a, 0x9d, 0x52, 0x71, 0x00, 0xe7, 0xb6, 0x07, 0x87, 0x15, 0xbb, 0x71, 0x2c, 0xdf,
+	0x40, 0x7f, 0xe9, 0x18, 0x1c, 0x82, 0x97, 0xc5, 0x01, 0x9b, 0xb1, 0xb0, 0x2d, 0xbc, 0x2c, 0xc6,
+	0x11, 0xb4, 0x73, 0x9d, 0x04, 0xde, 0x8c, 0x85, 0x7f, 0xc4, 0x57, 0x89, 0xff, 0x61, 0xb0, 0x4d,
+	0x23, 0x29, 0xe9, 0x75, 0x11, 0x07, 0xed, 0x19, 0x0b, 0x07, 0x62, 0x4f, 0xe0, 0x18, 0xba, 0xb9,
+	0x4e, 0x16, 0x71, 0xd0, 0xb1, 0x16, 0x0e, 0xf0, 0x13, 0xe8, 0x0b, 0x7a, 0x2b, 0x49, 0x1b, 0x44,
+	0xe8, 0xc8, 0x28, 0x27, 0x1b, 0x31, 0x10, 0xb6, 0xe6, 0x06, 0x7c, 0x41, 0x7a, 0xa7, 0xa4, 0xa6,
+	0xbd, 0x01, 0xfb, 0x61, 0xd0, 0x0c, 0xf5, 0x0e, 0x43, 0xa7, 0xe0, 0x17, 0xb4, 0xa5, 0xec, 0x9d,
+	0xdc, 0x44, 0xbe, 0xf8, 0xc6, 0x18, 0x40, 0xbf, 0x70, 0xd1, 0x76, 0x24, 0x5f, 0xd4, 0x90, 0x87,
+	0x30, 0x5a, 0x9b, 0x82, 0xa2, 0x3c, 0x93, 0x49, 0x3d, 0xdd, 0x18, 0xba, 0x5b, 0x55, 0x4a, 0x53,
+	0xa7, 0x5b, 0xc0, 0x4f, 0xa1, 0xb3, 0xca, 0x64, 0x82, 0xff, 0xa0, 0xa7, 0x4d, 0xa1, 0x5e, 0xa8,
+	0x92, 0x2b, 0x64, 0x75, 0x75, 0x5c, 0xbf, 0xfa, 0x60, 0x30, 0x5c, 0x36, 0x56, 0x8e, 0xb7, 0xf0,
+	0x77, 0x55, 0xea, 0xf4, 0x51, 0xdd, 0xb8, 0x5b, 0xe0, 0x64, 0xde, 0x7c, 0x94, 0x79, 0x75, 0x60,
+	0x1a, 0x1c, 0x0a, 0xf5, 0xaa, 0x78, 0x0b, 0x1f, 0x60, 0xd2, 0x70, 0x79, 0xca, 0x4c, 0xba, 0x36,
+	0x91, 0x29, 0xf5, 0xef, 0xfc, 0xee, 0x00, 0xee, 0x23, 0x43, 0x6e, 0x2d, 0x78, 0xb4, 0x73, 0x7a,
+	0xcc, 0x9c, 0xb7, 0x42, 0x76, 0xc9, 0x9e, 0x7b, 0xf6, 0x9b, 0x5d, 0x7f, 0x06, 0x00, 0x00, 0xff,
+	0xff, 0xd0, 0x06, 0x7a, 0xde, 0x93, 0x02, 0x00, 0x00,
 }
